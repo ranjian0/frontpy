3608fdf6b2cdc9c66c505932b3fc2af5b19fd6b3
ranjian0 <karanjaichungwa@gmail.com>
2021-06-13 00:29:27 +0300

    use live reload as server

--- front/__main__.py
+++ front/__main__.py
@@ -1,26 +1,16 @@
 import os
-import sys
 from pathlib import Path
-
-def run():
-    workdir = Path(os.curdir).absolute()
-
-    build_default_files(workdir)
-
-    # -- start the server
-    os.system('python -m http.server --directory build')
-
-def build_default_files(workdir):
-    """
-    index.py to generate the html files
-    app.py to generate the javascript
-    """
-    indexfile = workdir / 'index.py'
-    os.system(f'python {str(indexfile)}')
-
-    appfile = workdir / 'app.py'
-    os.system(f'python -m transcrypt -od {workdir / "build" / "js"} {str(appfile)}')
+from livereload import Server
+from livereload.server import shell
 
 
 if __name__ == "__main__":
-    run()
+    workdir = Path(os.curdir).absolute()
+
+    appfile =  workdir / 'app.py'
+    indexfile = workdir / 'index.py'
+
+    server = Server()
+    server.watch(str(indexfile), shell(f'python {indexfile}'))
+    server.watch(str(appfile), shell(f'python -m transcrypt -od {workdir / "build" / "js"} {str(appfile)}'))
+    server.serve(root=f'{str(workdir / "build")}', port=8000)
