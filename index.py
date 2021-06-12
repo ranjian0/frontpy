288bf1de3b7f200c12f8f2a19105d5cd97939a19
ranjian0 <karanjaichungwa@gmail.com>
2021-06-13 00:14:00 +0300

    add some more pages and extract nav

--- index.py
+++ index.py
@@ -3,6 +3,14 @@ from front.tags import *
 from front.cdn import bootstrap
 from front.document import document
 
+def navbar():
+    with nav(cls="navbar navbar-expand-lg navbar-dark bg-dark").add(ol(cls="navbar-nav me-auto mb-2 mb-lg-0")):
+        for i in ['home', 'about', 'contact']:
+            page = "index.html" if  i == 'home' else f"{i}.html"
+            li(a(i.title(), href=f'{page}', cls='nav-link'), cls="nav-item")
+
+
+
 @export
 def index():
     doc = document(title='Python Frontend')
@@ -12,10 +20,35 @@ def index():
         module("app")
 
     with doc:
-        with div(id='header').add(ol()):
-            for i in ['home', 'about', 'contact']:
-                li(a(i.title(), href='/%s.html' % i))
+        navbar()
+        button("Click Me", onclick="app.action()", cls="btn btn-primary")
 
+    return doc
+
+@export
+def about():
+    doc = document(title='Python Frontend')
+
+    with doc.head:
+        bootstrap()
+        module("app")
+
+    with doc:
+        navbar()
+        button("Click Me", onclick="app.action()", cls="btn btn-primary")
+
+    return doc
+
+@export
+def contact():
+    doc = document(title='Python Frontend')
+
+    with doc.head:
+        bootstrap()
+        module("app")
+
+    with doc:
+        navbar()
         button("Click Me", onclick="app.action()", cls="btn btn-primary")
 
     return doc
